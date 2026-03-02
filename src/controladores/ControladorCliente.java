22a18ba0d0d76e8bd9f51258d61bfd8361bc83b3
Anduquia17 <juan.marulanda.8318@eam.edu.co>
2026-03-02 17:15:31 -0500

    Se agregaron las funcionalidades y clases para la gestion de los clientes

---
+++ src/controladores/ControladorCliente.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controladores;
+
+import java.util.ArrayList;
+import modelo.Cliente;
+
+/**
+ *
+ * @author juanjoanduquia
+ */
+public class ControladorCliente {
+
+    private Cliente cliente;
+    private ArrayList<Cliente> listaDeclientes;
+
+    public ControladorCliente() {
+        this.cliente = cliente;
+        this.listaDeclientes = listaDeclientes;
+    }
+
+    public Cliente buscarCliente(String documento) {
+        for (int i = 0; i < listaDeclientes.size(); i++) {
+            if (listaDeclientes.get(i).getDocumento().equals(documento)) {
+                return listaDeclientes.get(i);
+            }
+
+        }
+        return null;
+
+    }
+
+    public boolean guardarCliente(Cliente cliente) {
+        Cliente aux = buscarCliente(cliente.getDocumento());
+        if (aux == null) {
+            listaDeclientes.add(cliente);
+            return true;
+
+        }
+        return false;
+    }
+
+    public boolean eliminarCliente(String documento) {
+        for (int i = 0; i < listaDeclientes.size(); i++) {
+            if (listaDeclientes.get(i).getDocumento().equals(documento));
+            listaDeclientes.remove(i);
+            return true;
+
+        }
+        return false;
+
+    }
+
+    public boolean prestarLibro(boolean tienePrestamo) {
+        if (tienePrestamo == true) {
+            return false;
+        }
+        return true;
+
+    }
+
+}
